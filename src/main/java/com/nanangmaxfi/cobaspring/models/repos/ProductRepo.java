798b41bdb1cd42d49f46f9e01d30df8e93da9eac
Nanang Mubarok <nanang_mubarok@saranaonline.com>
2021-10-07 15:14:33 +0700

    complete crud product

--- src/main/java/com/nanangmaxfi/cobaspring/models/repos/ProductRepo.java
+++ src/main/java/com/nanangmaxfi/cobaspring/models/repos/ProductRepo.java
@@ -1,9 +1,12 @@
 package com.nanangmaxfi.cobaspring.models.repos;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.nanangmaxfi.cobaspring.models.entities.Product;
 
 public interface ProductRepo extends CrudRepository<Product, Long>{
 
+	List<Product> findByNameContains(String name);
 }
--- src/main/java/com/nanangmaxfi/cobaspring/services/ProductService.java
+++ src/main/java/com/nanangmaxfi/cobaspring/services/ProductService.java
@@ -1,5 +1,44 @@
 package com.nanangmaxfi.cobaspring.services;
 
-public class ProductService {
+import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nanangmaxfi.cobaspring.models.entities.Product;
+import com.nanangmaxfi.cobaspring.models.repos.ProductRepo;
+
+@Service
+@Transactional
+public class ProductService {
+	
+	@Autowired
+	private ProductRepo productRepo;
+	
+	public Product save(Product product) {
+		return productRepo.save(product);
+	}
+	
+	public Product findOne(Long id) {
+		Optional<Product> product = productRepo.findById(id);
+		if(!product.isPresent()) {
+			return null;
+		}
+		return product.get();
+	}
+	
+	public Iterable<Product> findAll(){
+		return productRepo.findAll();
+	}
+	
+	public void removeOne(Long id) {
+		productRepo.deleteById(id);
+	}
+	
+	public List<Product> findByName(String name){
+		return productRepo.findByNameContains(name);
+	}
 }
