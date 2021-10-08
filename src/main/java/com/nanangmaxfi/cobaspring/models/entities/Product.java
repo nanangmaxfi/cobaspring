d8a4241d1d5edbee484b3ee08245d5917b85d902
Nanang Maxfi <n.maxfi.22@gmail.com>
2021-10-08 10:46:49 +0700

    add relasi entity jpa

--- src/main/java/com/nanangmaxfi/cobaspring/models/entities/Product.java
+++ src/main/java/com/nanangmaxfi/cobaspring/models/entities/Product.java
@@ -1,12 +1,17 @@
 package com.nanangmaxfi.cobaspring.models.entities;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
@@ -29,8 +34,18 @@ public class Product implements Serializable{
 	private String description;
 	
 	private double price;
-
 	
+	@ManyToOne
+	private Category category;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "tbl_product_supplier",
+			joinColumns = @JoinColumn(name = "product_id"),
+			inverseJoinColumns = @JoinColumn(name = "supplier_id"))
+	private Set<Supplier> suppliers;
+
+
 	public Product() {
 		super();
 	}
@@ -83,6 +98,26 @@ public class Product implements Serializable{
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	
+	public Category getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+
+	public Set<Supplier> getSuppliers() {
+		return suppliers;
+	}
+
+
+	public void setSuppliers(Set<Supplier> suppliers) {
+		this.suppliers = suppliers;
+	}
+	
 	
 	
 }
