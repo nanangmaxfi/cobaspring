d2359b55b37d63fda92270f4b79c721ee4eeb4a2
Nanang Maxfi <n.maxfi.22@gmail.com>
2021-10-15 03:55:38 +0700

    paging and sorting jpa

--- src/main/java/com/nanangmaxfi/cobaspring/controller/CategoryController.java
+++ src/main/java/com/nanangmaxfi/cobaspring/controller/CategoryController.java
@@ -1,9 +1,14 @@
 package com.nanangmaxfi.cobaspring.controller;
 
+import java.util.Arrays;
+
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nanangmaxfi.cobaspring.dto.CategoryData;
 import com.nanangmaxfi.cobaspring.dto.ResponseData;
+import com.nanangmaxfi.cobaspring.dto.SearchData;
 import com.nanangmaxfi.cobaspring.models.entities.Category;
 import com.nanangmaxfi.cobaspring.services.CategoryService;
 
@@ -74,6 +80,32 @@ public class CategoryController {
 		Category category = modelMapper.map(categoryData, Category.class);
 		responseData.setStatus(true);
 		responseData.setPayload(categoryService.save(category));
+		return ResponseEntity.ok(responseData);
+	}
+	
+	@PostMapping("/search/{size}/{page}")
+	public Iterable<Category> findByName(@RequestBody SearchData searchData, @PathVariable("size") int size,
+			@PathVariable("page") int page){
+		Pageable pageable = PageRequest.of(page, size);
+		return categoryService.findByName(searchData.getSearchKey(), pageable);
+	}
+	
+	@PostMapping("/search/{size}/{page}/{sort}")
+	public Iterable<Category> findByName(@RequestBody SearchData searchData, @PathVariable("size") int size,
+			@PathVariable("page") int page, @PathVariable("sort") String sort){
+		Pageable pageable = PageRequest.of(page, size, Sort.by("id"));
+		
+		if (sort.equalsIgnoreCase("desc")) {
+			pageable = PageRequest.of(page, size, Sort.by("id").descending());
+		}
+		return categoryService.findByName(searchData.getSearchKey(), pageable);
+	}
+	
+	@PostMapping("/batch")
+	public ResponseEntity<ResponseData<Iterable<Category>>> createBatch(@RequestBody Category[] categories){
+		ResponseData<Iterable<Category>> responseData = new ResponseData<>();
+		responseData.setPayload(categoryService.saveBatch(Arrays.asList(categories)));
+		responseData.setStatus(true);
 		return ResponseEntity.ok(responseData);
 	}
 }
--- src/main/java/com/nanangmaxfi/cobaspring/models/repos/CategoryRepo.java
+++ src/main/java/com/nanangmaxfi/cobaspring/models/repos/CategoryRepo.java
@@ -1,9 +1,12 @@
 package com.nanangmaxfi.cobaspring.models.repos;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.nanangmaxfi.cobaspring.models.entities.Category;
 
-public interface CategoryRepo extends CrudRepository<Category, Long>{
+public interface CategoryRepo extends PagingAndSortingRepository<Category, Long>{
 
+	Page<Category> findByNameContains(String name, Pageable pageable);
 }
